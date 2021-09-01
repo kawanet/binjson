5c77d976213bc01a1f028875da0be3032d9fd976
Yusuke Kawasaki <u-suke@kawa.net>
2021-09-02 01:47:07 +0900

    bufJSON.extend() test

--- test/600.bufjson.ts
+++ test/600.bufjson.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env mocha -R spec
 
 import {strict as assert} from "assert";
-import {bufJSON} from "../";
+import {bufJSON, handlers} from "../";
 
 const TITLE = __filename.split("/").pop();
 
 const DESCRIBE = ("undefined" !== typeof Buffer) ? describe : describe.skip;
+const toHex = (obj: Buffer) => [].map.call(obj, (v: number) => (0x100 | v).toString(16).substring(1)).join("-").toUpperCase();
 
 DESCRIBE(TITLE, () => {
 
@@ -15,9 +16,45 @@ DESCRIBE(TITLE, () => {
         const encoded = bufJSON.encode(source);
         assert.equal(Buffer.isBuffer(encoded), true);
 
-        const decoded = bufJSON.decode(encoded);
+        const decoded = bufJSON.decode<typeof source>(encoded);
         assert.equal(Buffer.isBuffer(decoded), false);
 
         assert.deepEqual(decoded, source);
+    });
+
+    it("bufJSON.encode()", () => {
+        const source = {data: Buffer.from([0x41, 0x42, 0x43])};
+
+        const encoded = bufJSON.encode(source);
+        assert.equal(Buffer.isBuffer(encoded), true);
+
+        const decoded = bufJSON.decode<typeof source>(encoded);
+        assert.equal(Buffer.isBuffer(decoded), false);
+        assert.equal(Buffer.isBuffer(decoded.data), true);
+
+        assert.equal(toHex(decoded.data), toHex(source.data));
+    });
+
+    it("bufJSON.extend()", () => {
+        const source = {date: new Date("2021-09-02")};
+
+        {
+            const encoded = bufJSON.encode(source);
+            assert.equal(Buffer.isBuffer(encoded), true);
+
+            const decoded = bufJSON.decode<typeof source>(encoded);
+            assert.equal(typeof decoded.date, "string");
+            assert.equal(decoded.date, source.date.toISOString());
+        }
+        {
+            const myJSON = bufJSON.extend({handler: handlers.Date});
+            const encoded = myJSON.encode(source);
+            assert.equal(Buffer.isBuffer(encoded), true);
+
+            const decoded = myJSON.decode<typeof source>(encoded);
+            assert.equal(typeof decoded.date, "object");
+            assert.equal(decoded.date instanceof Date, true);
+            assert.equal(decoded.date.toISOString(), source.date.toISOString());
+        }
     });
 });
